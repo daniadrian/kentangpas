58c38993584d46099b6799f6aab11c7ca954d8d9
Dani Adrian <daniadrian1311@gmail.com>
2025-08-09 01:11:59 +0700

    fix db

--- models/db.js
+++ models/db.js
@@ -1,19 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
+const { Pool } = require("pg");
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const connectionConfig = {};
+
+if (process.env.DATABASE_URL) {
+  connectionConfig.connectionString = process.env.DATABASE_URL;
+  connectionConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+} else {
+  connectionConfig.user = process.env.DB_USER;
+  connectionConfig.host = process.env.DB_HOST;
+  connectionConfig.database = process.env.DB_DATABASE;
+  connectionConfig.password = process.env.DB_PASSWORD;
+  connectionConfig.port = process.env.DB_PORT;
+}
+
+const pool = new Pool(connectionConfig);
 
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error('Error acquiring client', err.stack);
+    return console.error("Error saat koneksi ke database:", err.stack);
   }
-  console.log('Koneksi ke database PostgreSQL berhasil!');
+  console.log("Koneksi ke database PostgreSQL berhasil!");
   client.release();
 });
 
